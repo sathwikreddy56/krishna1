72252fc2de55ee18d0572c5988287c59c93268bc
sathwikreddy56 <sathwikreddy56@gmail.com>
2025-02-08 14:21:12 -0700

    inital front and back component

---
+++ src/app/Components/rightbar/rightbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rightbar',
+  imports: [],
+  templateUrl: './rightbar.component.html',
+  styleUrl: './rightbar.component.css'
+})
+export class RightbarComponent {
+
+}
